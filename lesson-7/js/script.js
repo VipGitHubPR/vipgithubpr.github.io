704d5e0a0c88a68c1dd077931e1a4c56f50cdeaf
VipGitHubPR <37479491+VipGitHubPR@users.noreply.github.com>
2018-05-11 21:35:59 +0300

    Add files via upload

---
+++ lesson-7/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.iconMenu').click(function() {
+    if($('.itemsMenu').is(':visible')) {
+      $('.itemsMenu').removeClass('showitems'); 
+    }
+    else {
+      $('.itemsMenu').addClass('showitems'); 
+    }   
+ }); 
+});
